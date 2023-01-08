dc072fde51ee0e8d6d87cd7f328d68fe2ee5d0bc
Andrei Serban <andrei.sserban@gmail.com>
2023-01-08 09:27:58 +0100

    Created company model

---
+++ models/Company.js
@@ -0,0 +1,19 @@
+const bcrypt = require("bcrypt");
+const mongoose = require("mongoose");
+
+const CompanySchema = new mongoose.Schema({
+    name: { type: String },
+    address: { type: String },
+    vat: { type: String },
+    contactName: { type: String },
+    contactPhone: { type: Number },
+    contactEmail: { type: String },
+    adminId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Admin"
+    },
+    createdAt: { type: Date, default: Date.now },
+});
+
+
+module.exports = mongoose.model("Company", CompanySchema, "companies");
