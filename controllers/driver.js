554ba3c28cb588056be55c800563fcd2b1f9202d
Andrei Serban <andrei.sserban@gmail.com>
2023-01-12 15:25:53 +0100

    in repair delete will remove photo from cloudinary too

--- controllers/driver.js
+++ controllers/driver.js
@@ -278,11 +278,8 @@ module.exports = {
   deleteRepair: async (req, res) => {
     try {
       let repair = await Repair.findById(req.params.id)
-      console.log(repair);
       await cloudinary.uploader.destroy(repair.cloudinaryId);
-
       await Repair.remove({ _id: req.params.id });
-      
       console.log("Deleted Repair");
       res.redirect(`/driver/repairs`);
     } catch (err) {
